d95fac689c1b173b62791fe99c8816d21c6ede0b
sanjanaa2102 <sanjanaprakash216@gmail.com>
2025-11-18 19:54:03 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
-// --- YOUR CONFIGURATION ---
+
 const firebaseConfig = {
   apiKey: "AIzaSyAFg4tcsLoZI50Gdomz8WOICs6TRjNTPh8",
   authDomain: "aethersim-project.firebaseapp.com",
@@ -12,14 +12,14 @@ const firebaseConfig = {
   measurementId: "G-NKV2B7SLH4"
 };
 
-// 1. Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// 2. Initialize Authentication
+
 export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 
-// 3. Export Helper Functions (This makes login easy)
+
 export const loginWithGoogle = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
