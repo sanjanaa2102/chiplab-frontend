db239816ec8c389dd2344dc2b1fde5cf58d68ea3
sanjanaa2102 <sanjanaprakash216@gmail.com>
2025-11-18 19:55:59 +0530

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,15 +1,15 @@
-// src/socket.js
+
 import { io } from 'socket.io-client';
 
-// This is the address of your backend server
+
 const URL = 'http://localhost:3001';
 
-// Create the socket connection
+
 const socket = io(URL, {
-  autoConnect: true, // Automatically connect
+  autoConnect: true,
 });
 
-// Log when we connect
+
 socket.on('connect', () => {
   console.log('Connected to WebSocket server!', socket.id);
 });
